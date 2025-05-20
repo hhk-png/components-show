09f6ae3701113e81610fe8ba5a9af4a843f441c5
hhk-png <fangluoabc@163.com>
2025-05-20 18:54:03 +0800

    chore

--- react-components/src/components-show/notification/index.ts
+++
@@ -1,6 +0,0 @@
-import Notification from './ForShow'
-
-export { ToastContextProvider } from './ToastContextProvider'
-export type * from './types'
-export { useToast } from './useToast'
-export default Notification
--- react-components/src/components-show/notification/useToast.ts
+++
@@ -1,4 +0,0 @@
-import { use } from 'react'
-import { ToastContext } from './ToastContext'
-
-export const useToast = () => use(ToastContext)
