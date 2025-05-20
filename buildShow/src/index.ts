dfc0b62e100f56b6b2d0ba5fa156d017e88f1701
hhk-png <fangluoabc@163.com>
2025-05-20 18:39:52 +0800

    fix: mkdir: MD React Vue

--- buildShow/src/index.ts
+++ buildShow/src/index.ts
@@ -1,7 +1,7 @@
 import path from "node:path"
 import componentsList from "./componentsList.ts"
 import type { FileTree } from "./types.d.ts"
-import { existsSync, readdirSync, readFileSync, statSync, writeFileSync } from "node:fs"
+import { existsSync, mkdirSync, readdirSync, readFileSync, statSync, writeFileSync } from "node:fs"
 import { fileURLToPath } from "node:url"
 import { resolveMd } from "./md2html.ts"
 import { codeToHtml } from "shiki"
@@ -66,14 +66,18 @@ async function saveComponents(componentsPath: string, targetDir: string) {
 const currFilePath = fileURLToPath(import.meta.url)
 const vueComponentsDir = path.resolve(currFilePath, '../../../vue-components/src/components-show')
 const targetVueJsonDir = path.resolve(currFilePath, '../../../react-components/public/Vue')
+mkdirSync(targetVueJsonDir, { recursive: true })
 saveComponents(vueComponentsDir, targetVueJsonDir)
 
 // transform react component Dir to json file
 const reactComponentsDir = path.resolve(currFilePath, '../../../react-components/src/components-show')
 const targetReactJsonDir = path.resolve(currFilePath, '../../../react-components/public/React')
+mkdirSync(targetReactJsonDir, { recursive: true })
 saveComponents(reactComponentsDir, targetReactJsonDir)
 
 // blog
+const targetMDDir = path.resolve(currFilePath, '../../../react-components/public/MD/images')
+mkdirSync(targetMDDir, { recursive: true })
 componentsList.forEach(resolveMd)
 
 // save components list
