f116c6bc053c3ccfe805fd76025b1a28acb5b00b
hhk-png <fangluoabc@163.com>
2025-06-09 14:04:57 +0800

    feat: leftmenu fallback

--- react-components/src/components/LeftMenu.tsx
+++ react-components/src/components/LeftMenu.tsx
@@ -15,18 +15,28 @@ const LeftMenu: React.FC<LeftMenuProps> = ({
 }) => {
   return (
     <div className={`flex flex-col gap-2 shrink-0 pb-3 ${className ? `${className}` : ''}`}>
-      {components.map(component => (
-        <button
-          type="button"
-          key={component}
-          onClick={() => onSelectComponent(component)}
-          className={`bg-blue-500 text-white p-3 rounded-lg m-1 mb-0 cursor-pointer 
-          hover:bg-blue-600 hover:shadow-lg active:bg-blue-700 transition duration-200
-          ${selectedComponent === component ? ' bg-blue-600 shadow-lg' : ''}`}
-        >
-          {component}
-        </button>
-      ))}
+      {components.length === 0 
+        ? (Array.from({length: 15}).map((_, index) => (
+            <button
+              type="button"
+              key={index}
+              className="bg-blue-500 text-white p-3 rounded-lg m-1 mb-0"
+            >
+              Loading...
+            </button>
+          )))
+        : components.map(component => (
+            <button
+              type="button"
+              key={component}
+              onClick={() => onSelectComponent(component)}
+              className={`bg-blue-500 text-white p-3 rounded-lg m-1 mb-0 cursor-pointer 
+              hover:bg-blue-600 hover:shadow-lg active:bg-blue-700 transition duration-200
+              ${selectedComponent === component ? ' bg-blue-600 shadow-lg' : ''}`}
+            >
+              {component}
+            </button>
+          ))}
     </div>
   )
 }
